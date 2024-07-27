f9adbe4ca7dd124ea61d11969c558355cba25f13
Stanley Horwood <stanley@hsjm.io>
2024-07-27 16:03:35 +0200

    feat(module-user): add `minimumPasswordComplexity` to `UserSettings`
    Introduced a new field `minimumPasswordComplexity` in the `UserSettings` entity to enhance security configurations. This field uses the zxcvbn algorithm to determine password complexity based on entropy, unique character count, length, and guesswork needed for a breach. This update also corrects the default values and naming consistency for the existing fields `minimumPasswordLength` and `isTotpRequired`.

--- packages/module-user/entities/UserSettings.ts
+++ packages/module-user/entities/UserSettings.ts
@@ -21,10 +21,20 @@ export class UserSettings extends Metadata {
    * The minimum password length. It determines the minimum length of the user password.
    * The user password must be at least this length.
    *
-   * @example 8
+   * @example 1
    */
-  @Column('int', { default: 8 })
+  @Column('int', { default: 1 })
     minimumPasswordLength: number
+
+  /**
+   * The minimum password complexity. As computed by the zxcvbn algorithm, it represents
+   * the entropy based on the number of unique characters, the length of the password and
+   * the number of guesses required to crack the password.
+   *
+   * @example 0
+   */
+  @Column('int', { default: 0 })
+    minimumPasswordComplexity: number
 
   /**
    * The TOTP code requirement status. It determines if the TOTP codes are required for
@@ -33,5 +43,5 @@ export class UserSettings extends Metadata {
    * @example false
    */
   @Column('boolean', { default: false })
-    isTOTPRequired: boolean
+    isTotpRequired: boolean
 }
