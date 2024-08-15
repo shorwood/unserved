0666f6b00633c6f825200274b71208352b026693
Stanley Horwood <stanley@hsjm.io>
2024-08-15 18:13:10 +0200

    chore(nuxt): streamline import logic and update compatibility version
    Refactored the module setup in `nuxt.ts` to simplify the import handling by removing conditionally differentiated import logic for components. This change makes the import process more straightforward by treating all imports uniformly. Additionally, the minimum compatible version of `nuxt` has been updated to `>=3.12.0` to align with the newest version requirements, ensuring compatibility with the latest features and improvements in the Nuxt framework.

--- packages/nuxt/nuxt.ts
+++ packages/nuxt/nuxt.ts
@@ -1,11 +1,11 @@
-import { addComponent, addImportsSources, defineNuxtModule } from '@nuxt/kit'
+import { addImportsSources, defineNuxtModule } from '@nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
     name: '@unserved/nuxt',
     configKey: '@unserved/nuxt',
     compatibility: {
-      nuxt: '>=3.0.0',
+      nuxt: '>=3.12.0',
       bridge: false,
     },
   },
@@ -13,13 +13,7 @@ export default defineNuxtModule({
     const imports = await import('./index.js')
     for (const name in imports) {
       if (name === 'default') continue
-
-      // --- By convention, exports starting with `Base` are components.
-      // --- Anything else is either a composable or utility function.
-      if (name.startsWith('Base'))
-        await addComponent({ name, export: name, filePath: `@unserved/nuxt/${name}` })
-      else
-        addImportsSources({ imports: [name], from: `@unserved/nuxt/${name}` })
+      addImportsSources({ imports: [name], from: `@unserved/nuxt/${name}` })
     }
   },
 })
