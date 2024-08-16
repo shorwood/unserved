172910dbb80ec6e4145a311efbab8b1de3168cf8
Stanley Horwood <stanley@hsjm.io>
2024-08-16 14:43:50 +0200

    chore(client): refine type definitions and add tests for type inference
    This update involves refining the type definitions in `types.ts` to improve the accuracy and flexibility of type inference across the client package. Changes include the use of `ModuleInstance` instead of `InferModule` and `Loose` instead of `PartialStrict` to handle optional properties more gracefully. Additionally, a comprehensive set of tests has been added to validate these type inferences effectively, ensuring that the types behave as expected in various scenarios. These tests utilize the `vitest` framework to simulate module interactions and check type correctness rigorously.

--- packages/client/types.ts
+++ packages/client/types.ts
@@ -1,5 +1,5 @@
-import type { ApplicationOrModule, EventStream, InferModule, RouteParser } from '@unserved/server'
-import type { Function, MaybePromise, PartialStrict, UnionMerge } from '@unshared/types'
+import type { ApplicationOrModule, EventStream, ModuleInstance, RouteParser } from '@unserved/server'
+import type { Function, Loose, MaybePromise, UnionMerge } from '@unshared/types'
 
 /**
  * Infer the routes of the application.
@@ -9,7 +9,7 @@ import type { Function, MaybePromise, PartialStrict, UnionMerge } from '@unshare
  * @example type AppRoute = ApplicationRoute<App> // Route
  */
 export type InferRoute<T extends ApplicationOrModule> =
-  InferModule<T> extends { routes: Record<string, infer Route> }
+  ModuleInstance<T> extends { routes: Record<string, infer Route> }
     ? Route extends Function<infer R> ? R : Route
     : never
 
@@ -20,7 +20,7 @@ export type InferRouteName<T extends ApplicationOrModule> =
 /** Infer the input data given a route name. */
 export type InferInput<T extends ApplicationOrModule, N extends InferRouteName<T>> =
   InferRoute<T> extends infer Route ? Route extends { name: N }
-    ? UnionMerge<PartialStrict<(
+    ? UnionMerge<Loose<(
       (Route extends { body: RouteParser<infer B> } ? B : never) |
       (Route extends { formData: RouteParser<infer F> } ? F : never) |
       (Route extends { parameters: RouteParser<infer P> } ? P : never) |
@@ -31,10 +31,9 @@ export type InferInput<T extends ApplicationOrModule, N extends InferRouteName<T
 
 /** Infer the payload data given a WebSocket route name. */
 export type InferPayload<T extends ApplicationOrModule, N extends InferRouteName<T>> =
-  InferRoute<T> extends infer Route
-    ? Route extends { name: N; parseMessage: RouteParser<infer Payload> }
-      ? Payload
-      : never
+  InferRoute<T> extends infer Route ? Route extends { name: N }
+    ? Route extends { parseMessage: RouteParser<infer U> } ? U : never
+    : never
     : never
 
 /** Infer the output data given a route name. */
@@ -44,3 +43,73 @@ export type InferOutput<T extends ApplicationOrModule, N extends InferRouteName<
       ? Output extends EventStream<infer U> ? AsyncIterable<U> : Output
       : never
     : never
+
+/* v8 ignore start */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+if (import.meta.vitest) {
+  const { ModuleBase, createRoute } = await import('@unserved/server')
+  type H3Event = import('h3').H3Event
+
+  // --- Mock module for testing.
+  class ModuleBlog extends ModuleBase {
+    routes = {
+      getPost: createRoute( {
+        name: 'GET /post/:id',
+        query: () => ({ name: 'John Doe' }) as { name: string | undefined },
+        parameters: () => ({ id: '123' }),
+      }, ({ query }) => `Hello, ${query.name}` ),
+
+      getPosts: createRoute( {
+        name: 'GET /posts',
+        query: () => ({ name: 'John Doe' }),
+      }, ({ query }) => `Hello, ${query.name}` ),
+
+      createPost: createRoute( {
+        name: 'POST /test',
+        query: () => ({ id: 'John Doe' }),
+        body: () => ({ name: 'John Doe' }),
+        formData: () => ({ file: 'file' }),
+      }, () => {}),
+
+      syncPost: createRoute( {
+        name: 'WS /post/:id',
+        parseMessage: () => ({ id: '123' }),
+      }, {}),
+    }
+  }
+
+  test('should infer the route of a module', () => {
+    type Result = InferRoute<typeof ModuleBlog>
+    expectTypeOf<Result>().toEqualTypeOf<
+      InstanceType<typeof ModuleBlog>['routes']['createPost'] |
+      InstanceType<typeof ModuleBlog>['routes']['getPost'] |
+      InstanceType<typeof ModuleBlog>['routes']['getPosts'] |
+      InstanceType<typeof ModuleBlog>['routes']['syncPost']
+    >()
+  })
+
+  test('should infer the route name of a module', () => {
+    type Result = InferRouteName<typeof ModuleBlog>
+    expectTypeOf<Result>().toEqualTypeOf<'GET /post/:id' | 'GET /posts' | 'POST /test' | 'WS /post/:id'>()
+  })
+
+  test('should infer the input of a GET route', () => {
+    type Result = InferInput<typeof ModuleBlog, 'GET /post/:id'>
+    expectTypeOf<Result>().toEqualTypeOf<{ id: string; name?: string | undefined }>()
+  })
+
+  test('should infer the input of a POST route', () => {
+    type Result = InferInput<typeof ModuleBlog, 'POST /test'>
+    expectTypeOf<Result>().toEqualTypeOf<{ id: string; name: string; file: string }>()
+  })
+
+  test('should infer the output of a route', () => {
+    type Result = InferOutput<typeof ModuleBlog, 'GET /post/:id'>
+    expectTypeOf<Result>().toEqualTypeOf<string>()
+  })
+
+  test('should infer the payload of a WebSocket route', () => {
+    type Result = InferPayload<typeof ModuleBlog, 'WS /post/:id'>
+    expectTypeOf<Result>().toEqualTypeOf<{ id: string }>()
+  })
+}
