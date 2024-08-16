3086bf087a2f03b4e94fb7a4999f07f5461cd481
Stanley Horwood <stanley@hsjm.io>
2024-08-16 14:45:38 +0200

    feat(server): refine type definitions and add testing for module utilities
    Refined various type definitions in the `server` package to enhance type inference capabilities and ensure compatibility with different module constructs. This includes adjustments to `ModuleInstance`, `ModuleConstructor`, `InferOptions`, and `InferEntities` types to more accurately reflect the intended usage and constraints.
    
    Additionally, introduced comprehensive test cases using `vitest` to verify the behavior of these types in practical scenarios. This helps in ensuring that the type utilities behave as expected when used with actual module and application instances, enhancing the reliability of the type system within the server package.

--- packages/server/types.ts
+++ packages/server/types.ts
@@ -1,13 +1,12 @@
-/* eslint-disable @typescript-eslint/consistent-type-imports */
 import type { Constructor, Function, UnionMerge } from '@unshared/types'
 import type { Application } from './createApplication'
-import { ModuleBase } from './createModule'
+import type { ModuleBase } from './createModule'
 
 /** A constructor of instance of a module. */
 export type ModuleLike = ModuleBase | typeof ModuleBase
 
 /** Inferable types of an application or module. */
-export type ApplicationOrModule = Application | ModuleLike
+export type ApplicationOrModule = Application | ModuleLike | typeof Application
 
 /** A permission object. */
 export interface PermissionObject {
@@ -24,7 +23,20 @@ export interface PermissionObject {
  * @example type Modules<typeof ModuleUser | ModuleStorage> // => ModuleUser | ModuleStorage
  */
 export type ModuleInstance<T extends ApplicationOrModule> =
-  T extends typeof ModuleBase ? InstanceType<T> : T
+  T extends Application<infer R> ? ModuleInstance<R>
+    : T extends typeof ModuleBase ? InstanceType<T> : T
+
+/**
+ * Infer the the module of the application.
+ *
+ * @template T The application to infer the routes from.
+ * @returns A union of the modules in the application.
+ * @example type AppModule = ApplicationModule<App> // ModuleHealth | ModuleUser
+ */
+export type ModuleConstructor<T extends ApplicationOrModule> =
+  T extends Application<infer R> ? R
+    : T extends typeof ModuleBase ? T
+      : never
 
 /**
  * Infer the options of an application or module instance.
@@ -32,9 +44,9 @@ export type ModuleInstance<T extends ApplicationOrModule> =
  * @template T The module class or constructor.
  */
 export type InferOptions<T extends ApplicationOrModule> =
-  Partial<UnionMerge<InferModule<T> extends Constructor<ModuleBase, [infer U | undefined]>
-    ? { [K in Exclude<keyof U, keyof ModuleBase>]-?: Exclude<U[K], Function | undefined> }
-    : Record<string, unknown>>>
+  ModuleConstructor<T> extends Constructor<ModuleBase, [infer U | undefined]>
+    ? Partial<{ [K in Exclude<keyof U, keyof ModuleBase>]-?: Exclude<U[K], Function | undefined> }>
+    : never
 
 /**
  * Given an application or module, infer the map of entities in the module or application.
@@ -45,12 +57,90 @@ export type InferOptions<T extends ApplicationOrModule> =
 export type InferEntities<T extends ApplicationOrModule> =
   UnionMerge<ModuleInstance<T> extends { entities: infer Entities } ? Entities : []>
 
-/**
- * Infer the the module of the application.
- *
- * @template T The application to infer the routes from.
- * @returns A union of the modules in the application.
- * @example type AppModule = ApplicationModule<App> // ModuleHealth | ModuleUser
- */
-export type InferModule<T extends ApplicationOrModule> =
-  T extends Application<infer R> ? ModuleInstance<R> : T
+/* v8 ignore start */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+if (import.meta.vitest) {
+  const { Application } = await import('./createApplication')
+  const { ModuleBase } = await import('./createModule')
+  const { Metadata } = await import('./Metadata')
+
+  class User extends ModuleBase { name = 'User' }
+  class ModuleUser extends ModuleBase {
+    constructor(options?: { name: string }) { super() }
+    entities = { User }
+  }
+  const moduleUser = new ModuleUser({ name: 'User' })
+  const application = new Application([ModuleUser])
+
+  describe('moduleInstance', () => {
+    it('should infer the module of a module constructor', () => {
+      type Result = ModuleInstance<typeof ModuleUser>
+      type Expected = InstanceType<typeof ModuleUser>
+      expectTypeOf<Result>().toEqualTypeOf<Expected>()
+    })
+
+    it('should infer the module of a module instance', () => {
+      type Result = ModuleInstance<typeof moduleUser>
+      type Expected = InstanceType<typeof ModuleUser>
+      expectTypeOf<Result>().toEqualTypeOf<Expected>()
+    })
+
+    it('should infer the module of an application instance', () => {
+      type Result = ModuleInstance<typeof application>
+      type Expected = InstanceType<typeof ModuleUser>
+      expectTypeOf<Result>().toEqualTypeOf<Expected>()
+    })
+  })
+
+  describe('inferModule', () => {
+    it('should infer the module of a module constructor', () => {
+      type Result = ModuleConstructor<typeof ModuleUser>
+      expectTypeOf<Result>().toEqualTypeOf<typeof ModuleUser>()
+    })
+
+    it('should infer the module of a module instance', () => {
+      type Result = ModuleConstructor<typeof moduleUser>
+      expectTypeOf<Result>().toEqualTypeOf<never>()
+    })
+
+    it('should infer the module of an application instance', () => {
+      type Result = ModuleConstructor<typeof application>
+      expectTypeOf<Result>().toEqualTypeOf<typeof ModuleUser>()
+    })
+  })
+
+  describe('inferOptions', () => {
+    it('should infer the options of a module constructor', () => {
+      type Result = InferOptions<typeof ModuleUser>
+      expectTypeOf<Result>().toEqualTypeOf<{ name?: string }>()
+    })
+
+    it('should infer the options of a module instance', () => {
+      type Result = InferOptions<typeof moduleUser>
+      expectTypeOf<Result>().toEqualTypeOf<{}>()
+    })
+
+    it('should infer the options of an application instance', () => {
+      type Result = InferOptions<typeof application>
+      expectTypeOf<Result>().toEqualTypeOf<{ name?: string }>()
+    })
+  })
+
+  describe('inferEntities', () => {
+    it('should infer the entities of a module constructor', () => {
+      type Result = InferEntities<typeof ModuleUser>
+      expectTypeOf<Result>().toEqualTypeOf<{ User: typeof User }>()
+    })
+
+    it('should infer the entities of a module instance', () => {
+      type Result = InferEntities<typeof moduleUser>
+      expectTypeOf<Result>().toEqualTypeOf<{ User: typeof User }>()
+    })
+
+    it('should infer the entities of an application instance', () => {
+      type Result = InferEntities<typeof application>
+      expectTypeOf<Result>().toEqualTypeOf<{ User: typeof User }>()
+    })
+  })
+}
