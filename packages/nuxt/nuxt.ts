e51e2e46dbb04e0636aaaed48588bf93f63da4e4
Stanley Horwood <stanley@hsjm.io>
2024-08-19 22:25:18 +0200

    feat(nuxt): enhance Vite integration in Nuxt module setup
    Enhanced the Vite configuration integration within the Nuxt module setup. Now, the module ensures the Vite config is properly defined and dynamically includes dependencies from the `@unserved/nuxt` namespace to optimize dependency handling. This improvement aims to streamline the building process and reduce potential issues with missing or unoptimized dependencies. Additionally, added `@nuxt/schema` as a dependency to align with updated Nuxt standards.

--- packages/nuxt/nuxt.ts
+++ packages/nuxt/nuxt.ts
@@ -9,10 +9,18 @@ export default defineNuxtModule({
       bridge: false,
     },
   },
-  async setup() {
+  async setup(_, { options }) {
+
+    // --- Ensure the Vite config is defined.
+    options.vite = options.vite ?? {}
+    options.vite.optimizeDeps = options.vite.optimizeDeps ?? {}
+    options.vite.optimizeDeps.include = options.vite.optimizeDeps.include ?? []
+
+    // --- Add the imports to the Vite config.
     const imports = await import('./index.js')
     for (const name in imports) {
       if (name === 'default') continue
+      options.vite.optimizeDeps.include.push(`@unserved/nuxt/${name}`)
       addImportsSources({ imports: [name], from: `@unserved/nuxt/${name}` })
     }
   },
