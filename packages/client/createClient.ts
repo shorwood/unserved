bf1a1144469b3a0748e2604a74b730bd556f65b4
Stanley Horwood <stanley@hsjm.io>
2024-11-28 14:50:06 +0100

    chore(client): add `RouteRequestData` type to improve type encapsulation
    Enhanced type encapsulation for route request data by introducing the `RouteRequestData` type. This new type combines `RouteRequestBody`, `RouteRequestParameters`, and `RouteRequestQuery` using `UnionMerge` to streamline type handling across different modules. This change aims to simplify the management of request data types and improve the consistency of type usage throughout the client package.

--- packages/client/createClient.ts
+++ packages/client/createClient.ts
@@ -4,7 +4,7 @@ import type { EventStream } from '@unserved/server/utils'
 import type { Client } from '@unshared/client'
 import type { FetchMethod, RequestOptions } from '@unshared/client/utils'
 import type { ConnectOptions } from '@unshared/client/websocket'
-import type { Loose, MaybeFunction, ObjectLike } from '@unshared/types'
+import type { Loose, MaybeFunction, ObjectLike, UnionMerge } from '@unshared/types'
 import { createClient as createBaseClient } from '@unshared/client/createClient'
 
 /**************************************************************/
@@ -22,15 +22,22 @@ export type RouteByName<T, N extends RouteName<T> = RouteName<T>> =
     : never
 
 export type RouteRequestQuery<T, N extends RouteName<T>> =
-  RouteByName<T, N> extends { parseQuery: Parser<infer U extends ObjectLike> } ? Loose<U> : ObjectLike
+  RouteByName<T, N> extends { parseQuery: Parser<infer U extends ObjectLike> } ? Loose<U> : never
 
 export type RouteRequestParameters<T, N extends RouteName<T>> =
-  RouteByName<T, N> extends { parseParameters: Parser<infer U extends ObjectLike> } ? Loose<U> : ObjectLike
+  RouteByName<T, N> extends { parseParameters: Parser<infer U extends ObjectLike> } ? Loose<U> : never
 
 export type RouteRequestBody<T, N extends RouteName<T>> =
   RouteByName<T, N> extends { parseBody: Parser<infer U> } ? (U extends ObjectLike ? Loose<U> : U)
     : RouteByName<T, N> extends { parseFormData: Parser<infer U extends ObjectLike> } ? U
       : never
+
+export type RouteRequestData<T, N extends RouteName<T>> =
+  Loose<UnionMerge<
+    | RouteRequestBody<T, N>
+    | RouteRequestParameters<T, N>
+    | RouteRequestQuery<T, N>
+  >>
 
 export type RouteResponseData<T, N extends RouteName<T>> =
   RouteByName<T> extends infer Route
