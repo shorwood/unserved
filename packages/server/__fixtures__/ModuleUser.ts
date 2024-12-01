f5bc641379574446325366b9350658d5a1e9bafe
Stanley Horwood <stanley@hsjm.io>
2024-12-01 06:18:36 +0100

    chore(server): improve `ModuleUser` example in fixtures
    Added routing example to `ModuleUser` fixture to demonstrate how routes can be defined using `createHttpRoute`. This includes parsing of query parameters and responding with user data. The example enhances clarity and provides a practical demonstration for developers working with the server module.

--- packages/server/__fixtures__/ModuleUser.ts
+++ packages/server/__fixtures__/ModuleUser.ts
@@ -1,6 +1,16 @@
+import { createHttpRoute } from '../createHttpRoute'
 import { ModuleBase } from '../createModule'
 import { User } from './User'
 
 export class ModuleUser extends ModuleBase {
   entities = { User }
+  routes = {
+    getUser: () => createHttpRoute({
+      name: 'GET /api/users/:id',
+      parseQuery: ({ id }) => ({ id: Number(id) }),
+    }, ({ query }) => ({
+      id: Number(query.id),
+      name: 'John Doe',
+    })),
+  }
 }
